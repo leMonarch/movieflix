22ca11d85c53100537728ee2113df713529954d6
Louis-Etienne Lemire <louis.lemonarch@gmail.com>
2025-05-05 13:01:12 -0400

    Tutoriel terminé avec quelques touches CSS et img à faire

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,124 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
+import Search from './components/Search'
+import Spinner from './components/Spinner';
+import MovieCard from './components/MovieCard';
+import { useDebounce } from 'react-use';
+import { getTrendingMovies, updateSearchCount } from './appwrite';
 
-function App() {
-  const [count, setCount] = useState(0)
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
+
+const App = () => {
+
+  const [ searchTerm,  setSearchTerm ] = useState('');
+  const [ errorMessage, setErrorMesage ] = useState('');
+  const [ movieList, setMovieList ] = useState([]);
+  const [ trendingMovies, setTrendingMovies ] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(false);
+  const [ debouncedSearchTerm, setDebouncedSearchTerm ] = useState('');
+
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
+
+  const fetchMovies = async (query = '') => {
+    setIsLoading(true);
+    setErrorMesage('');
+    try{
+      const endpoint = query 
+      ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+    
+      if(!response.ok){
+        throw new Error('Failed to fetch Movies');
+      }
+    
+      const data = await response.json();
+      console.log(data)
+      if(data.Response === false){
+        setErrorMesage(data.Error || 'Failed to fetch movies')
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || [])
+      if(query && data.results.length > 0){
+        updateSearchCount(query, data.results[0]);
+      }
+    } catch(error){
+      console.error(`Error fetching movies: ${error}`)
+      setErrorMesage("Error fetching Movies, try again later!")
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const loadTrendingMovies = async () => {
+    try{
+      const movies = await getTrendingMovies();
+      setTrendingMovies(movies);
+      console.log(movies)
+    } catch(error){
+      console.error(`Error fetching trending movies: ${error}`);
+    }
+  }
+
+  useEffect(()=>{
+    fetchMovies(debouncedSearchTerm);
+  },[debouncedSearchTerm])
+
+  useEffect(()=>{
+    loadTrendingMovies();
+  },[])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className='pattern'/>
+      <div className='wrapper'>
+        <header>
+          <img src="./hero.svg" alt="Hero Banner" />
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        </header>
+
+        {trendingMovies.length > 0 && (
+          <section className='trending'>
+            <h2>Trending Movies</h2>
+            <ul>
+              {trendingMovies.map((movie, index) => (
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  
+                  
+                  <img src={movie.poster_url} alt={movie.title} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
+        <section className='all-movies'>
+          <h2>All Movies</h2>
+          {isLoading ? (
+            <Spinner/>
+          ) : errorMessage ? (
+            <p className='text-red-500'>{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie}/>
+              ))}
+            </ul>
+          )}
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
---
+++ src/components/Search.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Search = ({searchTerm, setSearchTerm}) => {
+  return (
+    <div className='search'>
+        <div>
+            <img src="./search.svg" alt="search" />
+            <input 
+                    type="text" 
+                    value={searchTerm}
+                    placeholder='Search throught thousands of movies'
+                    onChange={(e) => setSearchTerm(e.target.value)}
+            />
+        </div>
+        
+    </div>
+  )
+}
+
+export default Search
